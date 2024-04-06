52064677ba2cc4d2c78782d296868d22e4fa739d
Anfro <aridding18@gmail.com>
2024-04-06 12:08:16 -0400

    update env

--- packages/nextjs/services/useGoogleMapsLoader.js
+++ packages/nextjs/services/useGoogleMapsLoader.js
@@ -6,7 +6,7 @@ const libraries = ["places", "geometry"]; // Combine all the libraries you need 
 const useGoogleMapsLoader = () => {
   const { isLoaded, loadError } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     libraries,
   });
 
